70be4b188bb0712495fe1f6c66129ef23a79ae52
Monster <coolpest@gmail.com>
2020-09-20 11:13:19 +0800

    dispatch

--- src/store/actions.js
+++ src/store/actions.js
@@ -1,8 +1,8 @@
+// import {GetUserListApi} from "../service/userApi";
 
 export default {
   async Get_User_List ({commit}, payload) {
     console.log("Get_User_List actions")
-
     // try {
     //   await GetUserListApi(payload["current_page"], payload["page_size"]).then(res => {
     //     if (res["entity"]["code"] === 200) {
